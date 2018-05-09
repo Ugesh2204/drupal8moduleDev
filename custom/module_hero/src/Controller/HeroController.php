ef3d6d1f9d84c5a4d4d3f019ef7c58258c8b0566
ugesh <ugesh.roopchand@hotmail.com>
2018-05-09 13:47:18 +0400

    adding animation to hero-list.html twig

--- custom/module_hero/src/Controller/HeroController.php
+++ custom/module_hero/src/Controller/HeroController.php
@@ -5,35 +5,28 @@ namespace Drupal\module_hero\Controller;
 use Drupal\Core\Controller\ControllerBase;
 
 /**
- * This is our hero controller
+ * This is our hero controller.
  */
-class HeroController extends ControllerBase{
+class HeroController extends ControllerBase {
 
-    public function heroList() {
-        /*Turn into array */
-        $heroes = [
+  public function heroList() {
 
-            ['name' => 'Hulk'],
-            ['name' => 'Thor'],
-            ['name' => 'Iron Man'],
-            ['name' => 'Wolverine'],
-            ['name' => 'Captain America']
-            
-        ];
+    $heroes = [
+      ['name' => 'Hulk'],
+      ['name' => 'Thor'],
+      ['name' => 'Iron Man'],
+      ['name' => 'Luke Cage'],
+      ['name' => 'Black Widow'],
+      ['name' => 'Daredevil'],
+      ['name' => 'Captain America'],
+      ['name' => 'Wolverine']
+    ];
 
-        /*Let concatonate them so that we could print */
+    return [
+      '#theme' => 'hero_list',
+      '#items' => $heroes,
+      '#title' => $this->t('Our wonderful heroes list'),
+    ];
 
-        $ourHeroes = '';
-        foreach($heroes as $hero) {
-            $ourHeroes .= '<li>' .$hero['name'] . '</li>';
-        }
-
-        /* Render an Array */
-
-        return [
-            '#type' => 'markup',
-            '#markup' => '<h4>' . $this->t('These are the best voted heroes') .'<h4><ol>'. $ourHeroes . '</ol>',
-
-        ];
-    }
+  }
 }
