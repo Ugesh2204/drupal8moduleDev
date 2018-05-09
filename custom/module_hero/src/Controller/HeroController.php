8da8119caf28bc937dfedab5e11e28eab0ebd212
ugesh <ugesh.roopchand@hotmail.com>
2018-05-09 10:06:18 +0400

    Render array

--- custom/module_hero/src/Controller/HeroController.php
+++ custom/module_hero/src/Controller/HeroController.php
@@ -8,6 +8,30 @@ namespace Drupal\module_hero\Controller;
 class HeroController {
 
     public function heroList() {
-        $heroes = [];
+        /*Turn into array */
+        $heroes = [
+
+            ['name' => 'Hulk'],
+            ['name' => 'Thor'],
+            ['name' => 'Iron Man'],
+            ['name' => 'Wolverine'],
+            ['name' => 'Captain America']
+            
+        ];
+
+        /*Let concatonate them so that we could print */
+
+        $ourHeroes = '';
+        foreach($heroes as $hero) {
+            $ourHeroes .= '<li>' .$hero['name'] . '</li>';
+        }
+
+        /* Render an Array */
+
+        return [
+            '#type' => 'markup',
+            '#markup' => '<ol>'. $ourHeroes . '</ol>',
+
+        ];
     }
 }
