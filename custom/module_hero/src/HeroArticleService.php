2c17c0b74e7465decc4a5fbb6c3cd0a086b14ffe
ugesh <ugesh.roopchand@hotmail.com>
2018-05-09 19:21:09 +0400

    service created

--- custom/module_hero/src/HeroArticleService.php
+++ custom/module_hero/src/HeroArticleService.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace Drupal\module_hero;
+use Drupal\Core\Entity\Query\QueryFactory;
+use Drupal\Core\Entity\EntityManager;
 
 /**
  * our hero article service class
@@ -9,11 +11,26 @@ namespace Drupal\module_hero;
 class HeroArticleService {
 
     /**
+     * caching the variable
+     */
+
+    private $entityQuery;
+    private $entityManager;
+
+    public function __construct(QueryFactory $entityQuery, EntityManager $entityManager) {
+        $this->entityQuery = $entityQuery;
+        $this->entityManager = $entityManager;
+    }
+
+    /**
      * Method for getting  Articles, regarding Heroes
      */
     public function getHeroArticles() {
-        $articles = ['Hulk is green!', 'Flash is red!'];
         
-        return $articles;
+
+      $articleNids = $this->entityQuery->get('node')->condition('type', 'article')->execute();
+
+        
+        return $this->entityManager->getStorage('node')->loadMultiple($articleNids);
     }
 }
