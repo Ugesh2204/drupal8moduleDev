e0921ae2256936232e8aeb5317a80bec36906360
ugesh <ugesh.roopchand@hotmail.com>
2018-05-09 09:53:26 +0400

    HeroController.php created

---
+++ custom/module_hero/src/Controller/HeroController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\module_hero\Controller;
+
+/**
+ * This is our hero controller
+ */
+class HeroController {
+
+    public function heroList() {
+        $heroes = [];
+    }
+}
